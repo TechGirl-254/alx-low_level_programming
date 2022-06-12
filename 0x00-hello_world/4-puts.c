b2485081dbdd0eff384e08188cca22f2ab5cd879
TechGirl-254 <wammymuriu@gmail.com>
2022-06-12 14:33:14 -0700

    writing a program that prints text on stdout

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * This program prints a text on stdout
+ *
+ * Main -Entry point
+ *
+ * Return Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
