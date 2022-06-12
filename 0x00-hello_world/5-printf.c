054ad8d6be5102cc5851884ef4c777d55db02a79
TechGirl-254 <wammymuriu@gmail.com>
2022-06-12 15:17:04 -0700

    Compiling a program using -Wall in gcc

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * Main -prints the phrase with proper grammar,
+ *
+ * but the outcome is a piece of art,
+ *
+ * Return: 0 if exited correctly
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
