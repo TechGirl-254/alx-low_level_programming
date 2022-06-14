dcb4a54193ba74cbf0713335256eea49d4e6eb19
TechGirl-254 <wammymuriu@gmail.com>
2022-06-14 06:42:51 -0700

    Creating main.h and writing putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - This prints the text _putchar
+ *
+ * Return: Always gives 0 when execution is successful
+ */
+int main(void)
+{
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
+	return (0);
+}
