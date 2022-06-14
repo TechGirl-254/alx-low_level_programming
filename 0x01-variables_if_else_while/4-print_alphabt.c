7acd7eb0574ed49d7f19a70bde86dfbc1e69f6f6
TechGirl-254 <wammymuriu@gmail.com>
2022-06-13 21:27:43 -0700

    Printing lowercase alphabet excluding p and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - This prints loweercase characters excluding p and e
+ *
+ * Return: Always gives 0 when the execution is successful
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 97; ch <= 122; ch++)
+		if ((ch != 101) && (ch != 112))
+			putchar(ch);
+	putchar(10);
+	return (0);
+}
