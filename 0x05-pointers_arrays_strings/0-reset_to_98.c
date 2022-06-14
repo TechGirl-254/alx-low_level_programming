5908b97630caabb77681d20c34fa9c0778b6fba0
TechGirl-254 <wammymuriu@gmail.com>
2022-06-14 16:59:15 -0700

    Printing a function that taks a ponter to an int as a parameter

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a function to an int as parameter
+ *
+ * @n: pointer to n to be updated
+ *
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
