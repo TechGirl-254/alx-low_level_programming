76111e81a8eb2a1c5d33c94b964076fba1677bf8
TechGirl-254 <wammymuriu@gmail.com>
2022-06-15 13:07:02 -0700

    First 98 numbers of the Fibbonnaci sequence

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,25 +7,35 @@
  */
 int main(void)
 {
-	int a;
-	int b;
-	int result;
-	int i;
+	unsigned long int i, j, k, j1, j2, k1, k2;
 
-	a = 1;
-	b = 2;
-	for (i = 1; i <= 98; i++)
+	j = 1;
+	k = 2;
+
+	printf("%lu", j);
+	for (i = 1; i < 91; i++)
 	{
-		if (i != 0 && i > 0)
-		{
-			printf("%d", a);
-			putchar(44);
-			putchar(32);
-			result = a + b;
-			a = b;
-			b = result;
-		}
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
 	}
+
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
+	}
+
 	printf("\n");
+
 	return (0);
 }
