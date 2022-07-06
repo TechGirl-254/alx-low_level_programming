331a4e093465475b93c7d1ba611694b55533ef2e
TechGirl-254 <wammymuriu@gmail.com>
2022-07-06 06:01:29 -0700

    Trying out argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Checks the code
+ *
+ * @argc: An int variable storing the number of arguements in the program
+ * Return: Always 0
+ */
+int main(int argc)
+{
+	printf("%d", argc);
+	printf("\n");
+	return (0);
+}
