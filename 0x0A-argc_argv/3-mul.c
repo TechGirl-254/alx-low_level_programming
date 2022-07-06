4f055590b456224536d50c073fe49dcaae6b396e
TechGirl-254 <wammymuriu@gmail.com>
2022-07-06 09:30:36 -0700

    Multiplying arguements

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Checks the code
+ *
+ * @argc: An int variable storing the number of arguements in the program
+ * @argv: A character array storing the program's strings
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int mul = 1;
+
+	if (argc != 3)
+		printf("Error\n");
+	return (1);
+	for (i = 1; i < argc; i++)
+		mul *= atoi(argv[i]);
+	printf("%d\n", mul);
+	return (0);
+}
