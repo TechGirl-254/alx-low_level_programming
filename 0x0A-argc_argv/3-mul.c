9073413cbed63fa36b39781ba8c4707e18bb6fb7
TechGirl-254 <wammymuriu@gmail.com>
2022-07-06 09:41:11 -0700

    Multiplying arguements

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,10 +14,12 @@ int main(int argc, char *argv[])
 	int mul = 1;
 
 	if (argc != 3)
+	{
 		printf("Error\n");
 	return (1);
+	}
 	for (i = 1; i < argc; i++)
-		mul *= atoi(argv[i]);
+			mul *= atoi(argv[i]);
 	printf("%d\n", mul);
 	return (0);
 }
