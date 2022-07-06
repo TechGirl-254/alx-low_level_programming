386d23941e49202d1e0a0bcdfb5244441217c8a9
TechGirl-254 <wammymuriu@gmail.com>
2022-07-06 06:48:11 -0700

    Trying out argc

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,9 +4,10 @@
  * main - Checks the code
  *
  * @argc: An int variable storing the number of arguements in the program
+ * @argv: A character array storing the program's strings
  * Return: Always 0
  */
-int main(int argc)
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d", argc);
 	printf("\n");
