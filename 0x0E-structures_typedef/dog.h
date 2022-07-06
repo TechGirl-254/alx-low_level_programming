f6db90bbd8f8d2eb758afbd4635e7c69a42601e7
TechGirl-254 <wammymuriu@gmail.com>
2022-07-06 02:16:49 -0700

    Using Structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - is a structure defining three elements
+ * @name: The name of the dog
+ * @age: The dog's age
+ * @owner: The dog owner's name
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
