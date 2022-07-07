dbbdb3a950f3ab120ef8a4d374038e010046a8a5
TechGirl-254 <wammymuriu@gmail.com>
2022-07-07 13:50:39 -0700

    Freing up memory usinf free and confirming using valgrind

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_grid -> removing memory allocation
+ * @grid: grid to be freed
+ * @height: height of matrix
+ * Return: nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
