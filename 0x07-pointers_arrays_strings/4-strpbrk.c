820d6b32060ae3fb77a02520f4c1a8920f82c2c4
TechGirl-254 <wammymuriu@gmail.com>
2022-08-04 09:21:58 -0700

    Finding a string inside another string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strpbrk - Searches for a string in any set of bytes
+ * @s: The haystack
+ * @accept: The needle
+ *
+ * Description My thought process about this is that
+ * there's a string(hastack) and within it you can
+ * find another smaller string(needle). To find the
+ * needle in the haystack, you have to start by
+ * finding the first character on the needle among
+ * the characters in the haystack.
+ *
+ * Return: A pointer to the byte in s that matches
+ * the byte in accept.
+ * otherwise, it returns null
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	char *p;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				j++;
+
+				p = &s[i];
+			}
+			else
+				break;
+		}
+	}
+	i++;
+
+	if (p == NULL)
+		return (NULL);
+	else
+		return (p);
+}
