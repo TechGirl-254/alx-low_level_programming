b8b20e9e95b048130e41daf273c3395ebfc4b993
TechGirl-254 <wammymuriu@gmail.com>
2022-08-04 09:42:55 -0700

    Correctin the source file

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -6,44 +6,19 @@
  * @s: The haystack
  * @accept: The needle
  *
- * Description My thought process about this is that
- * there's a string(hastack) and within it you can
- * find another smaller string(needle). To find the
- * needle in the haystack, you have to start by
- * finding the first character on the needle among
- * the characters in the haystack.
- *
- * Return: A pointer to the byte in s that matches
- * the byte in accept.
- * otherwise, it returns null
+ * Return: s (Success) 0 (Not Found)
  */
 char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
-	char *p;
 
-	i = 0;
-
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		j = 0;
-
-		while (accept[j] != '\0')
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
-			{
-				j++;
-
-				p = &s[i];
-			}
-			else
-				break;
+				return (&s[i]);
 		}
 	}
-	i++;
-
-	if (p == NULL)
-		return (NULL);
-	else
-		return (p);
+	return (0);
 }
