037a7c8e10deb8653da697cd96ca107d2fdafd39
TechGirl-254 <wammymuriu@gmail.com>
2022-09-21 01:15:07 -0700

    Writing a function that sums up integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - A function that finds the sum of two  integers
+ *
+ * int: The first integer
+ *
+ * int: The second interger
+ * Return: The sum of the integers
+ */
+int add(int a, int b)
+{
+	int x;
+	
+	x = a + b;
+	return (x);
+}
