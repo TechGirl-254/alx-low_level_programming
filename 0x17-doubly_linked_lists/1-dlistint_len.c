fcc7c16ef22880cc75df74364a65177f02235e13
TechGirl-254 <wammymuriu@gmail.com>
2022-10-13 15:37:47 -0700

    Calculating the number of elements in a doubly linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * print_dlistint - A function that prints all elements of a
+ * doubly linked list.
+ *
+ * @h: A pointer to the fist node of the doubly linked list
+ *
+ * Return: The number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int i;
+
+	const dlistint_t *ptr;
+
+	ptr = h;
+
+	i = 0;
+
+	while (ptr != NULL)
+		i += 1;
+	return (i);
+}
